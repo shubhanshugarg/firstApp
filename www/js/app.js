6e77f57571088527451d646d1d396b71985c2e10
rohanmyid <smartyrohan2007@gmail.com>
2015-07-31 20:01:56 +0530

    base64 to formdata

--- www/js/app.js
+++ www/js/app.js
@@ -972,6 +972,7 @@ var app = {
 
       // Get image handle
       //
+      $scope.imageBlob = dataURItoBlob(imageData);
       var smallImage = document.getElementById('smallImage');
 
       // Unhide image elements
@@ -983,11 +984,31 @@ var app = {
       //
       smallImage.src = "data:image/jpeg;base64," + imageData;
     }
+    $scope.dataURItoBlob=function (dataURI) {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+        byteString = atob(dataURI.split(',')[1]);
+    else
+        byteString = unescape(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], {type:mimeString});
+}
         $scope.submitForm = function () {
             var heading = $scope.subject;
             var description = $scope.message;
             var url = $scope.url;
             var fbUrl = $scope.fbUrl;
+            var imageBlob=$scope.imageBlob;
             var categories = '';
             //for loop all categories time
             /*var i = 0;
@@ -1050,7 +1071,8 @@ var app = {
             fd.append('noticeUrl', url);
             fd.append('noticeFBUrl', fbUrl);
             //fd.append('infoState', 'APPROVED');
-            fd.append('noticeImageFile', imgUri);
+            //fd.append('noticeImageFile', imgUri);
+            fd.append('noticeImageFile', imageBlob);
 
             var locationOrigin = "http://collegeboard-env2.elasticbeanstalk.com";
             $http.post(locationOrigin + "/noticeInfo/postNotice", fd, {
