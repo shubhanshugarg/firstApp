8a86c4c6bebd6b1900616eeeffd57be18fe2e615
rohanmyid <smartyrohan2007@gmail.com>
2015-08-02 00:26:05 +0530

    reduced size photo selection

--- www/js/app.js
+++ www/js/app.js
@@ -1027,6 +1027,33 @@ var app = {
                 sourceType: source });
 
         };
+        $scope.loadImageFile = function(){
+
+       var oFReader = new FileReader(), rFilter = /^(?:image\/bmp|image\/cis\-cod|image\/gif|image\/ief|image\/jpeg|image\/jpeg|image\/jpeg|image\/pipeg|image\/png|image\/svg\+xml|image\/tiff|image\/x\-cmu\-raster|image\/x\-cmx|image\/x\-icon|image\/x\-portable\-anymap|image\/x\-portable\-bitmap|image\/x\-portable\-graymap|image\/x\-portable\-pixmap|image\/x\-rgb|image\/x\-xbitmap|image\/x\-xpixmap|image\/x\-xwindowdump)$/i;
+
+        oFReader.onload = function (oFREvent) {
+
+          var img=new Image();
+          img.onload=function(){
+              document.getElementById("originalImg").src=img.src;
+              var canvas=document.createElement("canvas");
+              var ctx=canvas.getContext("2d");
+              canvas.width=img.width/2;
+              canvas.height=img.height/2;
+              ctx.drawImage(img,0,0,img.width,img.height,0,0,canvas.width,canvas.height);
+              document.getElementById("uploadPreview").src = canvas.toDataURL();
+              var imageData=canvas.toDataURL();
+              $scope.imageBlob = $scope.dataURItoBlob(imageData);
+          }
+          img.src=oFREvent.target.result;
+        };
+                
+        if (document.getElementById("uploadImage").files.length === 0) { return; }
+          var oFile = document.getElementById("uploadImage").files[0];
+          if (!rFilter.test(oFile.type)) { alert("You must select a valid image file!"); return; }
+          oFReader.readAsDataURL(oFile);
+
+        };
         $scope.submitForm = function () {
             var heading = $scope.subject;
             var description = $scope.message;
